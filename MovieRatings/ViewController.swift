8cb86946295fc51d7c9a971075d631230f125a42
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-11-03 20:37:15 -0500

    Initial Commit

---
+++ MovieRatings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRatings
+//
+//  Created by Cole Fekert on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
